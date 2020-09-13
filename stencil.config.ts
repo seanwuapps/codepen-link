7cdbf066d8eff3b32d76d7afe086e2a61be7e5cb
Sean Wu <seanwuapps@gmail.com>
2020-09-13 21:32:51 +1000

    init import

---
+++ stencil.config.ts
@@ -0,0 +1,23 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'codepen-link',
+  plugins: [sass()],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
